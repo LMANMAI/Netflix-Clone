3cca1ab3d697212311e5eccb9255c36b873cfe92
Lucas Manuel <lucas_sknr.010@live.com>
2021-03-06 19:41:13 -0300

    Agregue el divisor y reutilize el componente de la seccion

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './styles.scss';
-import Hero from './auxiliars/Hero/Hero'
+import Hero from './auxiliars/Hero';
+import Divider from './auxiliars/Divider';
+import LandingSection from './auxiliars/Section';
+import FAQ from './auxiliars/FAQ';
 
 function LandingPage() {
     return (
         <div className="LandingPage">
             <Hero />
+            <Divider />
+            <LandingSection 
+                image="/images/home-tv.jpg"
+                imageposition="rigth"
+                tittle="Disfrutá en tu tele."
+                subtittle={<>Mirá en smart TV, PlayStation, Xbox, <br/> Chromecast, Apple TV, reproductores de Blu-ray y más.</>}
+            />
+            <Divider />
+            <LandingSection 
+                image="/images/home-mobile.jpg"
+                imageposition="left"
+                tittle={<>Descargá tus series <br/>para mirarlas offline.</>}
+                subtittle={<>Guardá tu contenido favorito y tené siempre <br/> algo para mirar.</>}
+            />
+            <Divider />
+            <LandingSection 
+                image="/images/home-imac.jpg"
+                imageposition="rigth"
+                tittle="Disfrutá donde quieras."
+                subtittle={<>Películas y series ilimitadas en tu teléfono, <br/> tablet, computadora y tele sin costo extra.</>}
+            />
+            <Divider />
+            <FAQ/>
+            <Divider />
         </div>
     )
 }
