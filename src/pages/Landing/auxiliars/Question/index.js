488f01af97cb01fa8e4b4b711aab40e9a196216f
Lucas Manuel <lucas_sknr.010@live.com>
2021-03-07 14:19:58 -0300

    Componente individual de la pregunta

--- src/pages/Landing/auxiliars/Question/index.js
+++ src/pages/Landing/auxiliars/Question/index.js
@@ -1,7 +1,7 @@
 import React,{ useState } from 'react';
 import './styles.scss';
 
-function Question() {
+function Question({tittle, answer}) {
     const [open , SetOpen ] = useState(false);
     const toggleQuestion = () => {
         SetOpen(!open);
@@ -10,16 +10,13 @@ function Question() {
         <div className="Question">
             <div  onClick={toggleQuestion} className="Question_tittle box">
                 <h2 className="Question_tittle-text">
-                    ¿Que es Netflix?
+                    {tittle}
                 </h2>
                 <span className="Question_tittle-text">{open ? "X" : "+"}</span>
             </div>
         {open && 
            (<div className="Question_answer box">
-                <p className="Question_answer-text">
-                Netflix es un servicio de streaming que ofrece una gran variedad de películas, series y documentales premiados en casi cualquier pantalla conectada a internet.<br/><br/>
-                Todo lo que quieras mirar, sin límites ni comerciales, a un costo muy accesible. Siempre hay algo nuevo por descubrir, ¡y todas las semanas se agregan más películas y series!
-                </p>
+                <p className="Question_answer-text">{answer}</p>
             </div>
             )}
         </div>
