b0d74b3000234240cba39c72dc1fc1c94bc291ce
Lucas Manuel <lucas_sknr.010@live.com>
2021-03-08 17:57:10 -0300

    Componente del Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+import { GiEarthAmerica } from "react-icons/gi";
+
+const Container = styled.div`
+  max-width: 1000px;
+  color: white;
+  background-color: rgba(0, 0, 0, 0.75);
+  margin: 0 auto;
+  padding: 30px;
+`;
+const Tittle = styled.p`
+  color: #757575;
+  max-width: 1000px;
+  margin-bottom: 25px;
+`;
+const LinkContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
+const LinkItem = styled.a`
+  color: #757575;
+  max-width: 1000px;
+  cursor: pointer;
+  font-size: 12px;
+  margin-bottom: 2rem;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+const LanguageContainer = styled.div`
+  color: #757575;
+  display: flex;
+  align-items: center;
+  border: 1px solid #333;
+  border-radius: 5px;
+  width: fit-content;
+  padding: .8rem .45rem;
+`;
+const LanguageSelect = styled.select`
+  margin-left: 14px;
+  background-color: #000;
+  outline: none;
+  color: #757575;
+  border: none; 
+`;
+const DATA = [
+  "Preguntas Frecuentes",
+  "Centro de ayuda",
+  "Terminos de Uso",
+  "Privacidad",
+  "Preferencia de Cookies",
+  "informacion Corporativa",
+];
+function Footer() {
+  return (
+    <Container>
+      <Tittle>¿Preguntas? Llamá al 800-345-1375</Tittle>
+      <LinkContainer>
+        {React.Children.toArray(
+          DATA.map((dato) => <LinkItem>{dato}</LinkItem>)
+        )}
+      </LinkContainer>
+      <LanguageContainer>
+        <GiEarthAmerica />
+        <LanguageSelect name="select">
+          <option value="español">Español</option>
+          <option value="ingles">Ingles</option>
+        </LanguageSelect>
+      </LanguageContainer>
+    </Container>
+  );
+}
+
+export default Footer;
