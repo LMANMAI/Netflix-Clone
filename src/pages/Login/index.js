4b17caa36e03d4909bfc4a5a942c59c49cddbfae
Lucas Manuel <lucas_sknr.010@live.com>
2021-03-08 18:02:23 -0300

    Preparacion del Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { Header } from "../../components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+const Background = styled.img`
+  position: absolute;
+  width: 100%;
+  height:100%;
+  z-index: -1;
+  object-fit: cover;
+  filter: brightness(.5);
+`;
 
 function LoginPage() {
-    return (
-        <div>
-            desde el login
-        </div>
-    )
+  return (
+    <Container>
+      <Background src="/images/home-bg.jpg" alt="background" />
+      <Header showButton={false} />
+    </Container>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
