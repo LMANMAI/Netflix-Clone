a8387776196a570409d2a2e63491a6e4ff1184ef
Lucas Manuel <lucas_sknr.010@live.com>
2021-03-11 19:45:02 -0300

    Home was started

--- src/pages/Home/auxiliars/Header.jsx
+++ src/pages/Home/auxiliars/Header.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
+import { AuthContext } from "../../../provider/AuthProvider";
+import { BsSearch, BsFillBellFill, BsFillGiftFill } from "react-icons/bs";
+import { GoTriangleDown } from "react-icons/go";
 const HeaderContainer = styled.div`
   z-index: 1;
   width: 100%;
@@ -12,35 +15,76 @@ const HeaderContainer = styled.div`
 
 const ArrowWrapper = styled.div`
   display: flex;
+  align-items: center;
+  color: white;
 `;
-const Logo = styled.div`
-  width: 150px;
-  height: 45px;
+const Logo = styled.img`
+  width: 120px;
+  height: 40px;
+  margin-right: 20px;
   object-fit: cover;
 `;
 const Link = styled.p`
-    font-weight: ${(props) => (props.selected === true ?"700" : "300")};
-    color: white;
-    transition: color 300ms;
-    cursor: pointer;
-    &:hover{
-        color: gray;
-    }
+  font-weight: ${(props) => (props.selected === true ? "700" : "300")};
+  transition: color 300ms;
+  cursor: pointer;
+  font-size: 15px;
+  padding: 0 10px;
+  &:hover {
+    color: gray;
+  }
+`;
+const AvatarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 32px;
+    height: 32px;
+    border-radius: 4px;
+    object-fit: cover;
+    margin-right: 8px;
+  }
+`;
+const RightWrapper = styled(ArrowWrapper)`
+  width: 13%;
+  justify-content: space-between;
+  svg, p{
+      cursor: pointer;
+  }
+  p{
+      font-size: 12px;
+  }
 `;
 const DATA = [
-    "Inicio",
-    "Series",
-    "Peliculas",
-    "Novedades Populares",
-    "Mi Lista",
-  ];
+  "Inicio",
+  "Series",
+  "Peliculas",
+  "Novedades Populares",
+  "Mi Lista",
+];
+
 function Header() {
+  const authContext = useContext(AuthContext);
+  const { user } = authContext;
   return (
     <HeaderContainer>
-      <Logo alt="background image" src="/images/Netflix_Logo.png" />
       <ArrowWrapper>
-        {React.Children.toArray(DATA.map((link, index) => <Link selected={ index === 0 }>{link}</Link>))}
+        <Logo alt="background image" src="/images/Netflix_Logo.png" />
+        {React.Children.toArray(
+          DATA.map((link, index) => <Link selected={index === 0}>{link}</Link>)
+        )}
       </ArrowWrapper>
+
+      <RightWrapper>
+        <BsSearch />
+        <p>{user.username}</p>
+        <BsFillGiftFill />
+        <BsFillBellFill />
+        <AvatarContainer>
+          <img src={user.avatarUrl} alt={user.username} />
+          <GoTriangleDown />
+        </AvatarContainer>
+      </RightWrapper>
     </HeaderContainer>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,91 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "./auxiliars/Header";
+import { FiInfo } from "react-icons/fi";
+import { GrPlayFill } from "react-icons/gr";
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: #000;
 `;
+const Hero = styled.div`
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  height: 56.25vw;
+  width: 100%;
+`;
+const BHero = styled.video`
+  width: 100%;
+  object-fit: cover;
+  position: absolute;
+  filter: brightness(0.6);
+  z-index: -1;
+`;
+const FillContain = styled.div`
+  padding-top: 20%;
+  width: 95%;
+  margin: 0 auto;
 
+`;
+const Logo = styled.img``;
+const Bold = styled.p``;
+const Desc = styled.p``;
+const ButtonContainer = styled.div`
+  display: flex;
+  width: fit-content;
+`;
+const Button = styled.div`
+  padding: 0.8rem;
+  border-radius: 4px;
+  text-align: center;
+  padding-left: 2rem;
+  padding-right: 2.4rem;
+  width: fit-content;
+  margin: 1.5vw 0.75vw 0.1vw;
+  svg {
+    margin-right: 5px;
+  }
+`;
+const PlayButton = styled(Button)`
+  background-color: #fff;
+  margin-left: 1.5vw;
+`;
+const InfoButton = styled(Button)`
+  background-color: rgb(223 223 223 / 34%);
+  color: white;
+`;
 function HomePage() {
-  return <Container>
+  return (
+    <Container>
       <Header />
-  </Container>;
+      <Hero>
+        <BHero loop autoPlay>
+          <source src="/videos/HeroVideo.mp4" />
+        </BHero>
+        <FillContain>
+          <Logo src="/images/LogoSerieHero.webp" />
+          <Bold>Ve la temporada 1</Bold>
+          <Desc>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            quos vero, ea nisi blanditiis asperiores dolore nobis quibusdam
+            mollitia consequuntur optio illum odit voluptas fuga amet ex sed
+            veniam praesentium?
+          </Desc>
+          <ButtonContainer>
+            <PlayButton>
+              <GrPlayFill />
+              Reproducir
+            </PlayButton>
+            <InfoButton>
+              <FiInfo />
+              Más Información
+            </InfoButton>
+          </ButtonContainer>
+        </FillContain>
+      </Hero>
+    </Container>
+  );
 }
 
 export default HomePage;
